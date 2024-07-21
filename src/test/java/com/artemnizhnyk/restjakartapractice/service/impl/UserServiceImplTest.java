58a4dc178a900adc682bfe9cbff7a0c1b5270e59
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-07-21 21:54:46 +0200

    some practice

---
+++ src/test/java/com/artemnizhnyk/restjakartapractice/service/impl/UserServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.artemnizhnyk.restjakartapractice.service.impl;
+
+import com.artemnizhnyk.restjakartapractice.domain.model.User;
+import com.artemnizhnyk.restjakartapractice.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+
+
+    @Test
+    void getUserById_shouldReturnUserWithPassedId() {
+        //given
+        User user = new User(1L, "Artem", "12345", new ArrayList<>());
+        long passedUserId = 1;
+
+        //when
+        when(userRepository.getUserById(passedUserId)).thenReturn(user);
+        User userById = userService.getUserById(passedUserId);
+
+        //then
+        assertThat(userById).isEqualTo(user);
+    }
+}
