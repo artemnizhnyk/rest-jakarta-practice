a6cda6f8ec9066f55a9b145489deb1a1bdafd4a2
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-08-20 21:08:32 +0200

    TaskDto was added

--- src/main/java/com/artemnizhnyk/restjakartapractice/web/dto/TaskDto.java
+++ src/main/java/com/artemnizhnyk/restjakartapractice/web/dto/TaskDto.java
@@ -1,4 +1,21 @@
 package com.artemnizhnyk.restjakartapractice.web.dto;
 
+import com.artemnizhnyk.restjakartapractice.domain.model.task.Status;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Data
 public class TaskDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Status status;
+    private LocalDateTime expiresAt;
 }
